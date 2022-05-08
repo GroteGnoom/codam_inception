7713b95c2f1e1c75ad12e230659f0497f612d64a
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-05-08 16:28:17 +0200

    move a bit of wordpress from script to Dockerfile

--- srcs/wordpress/wp.sh
+++ srcs/wordpress/wp.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -x
-#echo "127.0.0.1 dnoom.42.fr" >> /etc/hosts #todo check if necessary
-chown -R www-data:www-data /var/www
-chmod -R 755 /var/www/
-mkdir -p /var/www/html
-#cp replaced by config create
-#cp /tmp/wp-config.php /var/www/html/
 if [ ! -f "/var/www/html/index.php" ]; then 
 	cd /var/www/html/
+	#you could do the core download in the building process, but this is more flexible, because now you can remove the volume
+	#wp core download doesn't seem to just download. it also sets some config which makes the current folder the wordpress root folder. Very confusing :(
 	wp core download --allow-root;
 	while ! echo "SHOW DATABASES;" | mariadb -hmariadb -uboss -p${WP_BOSS_PASSWORD}; do sleep 1; done
 	wp config create --dbname=WP --dbuser=boss --dbpass=${WP_BOSS_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root 
