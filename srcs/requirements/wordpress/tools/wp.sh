8b52fe3195bfb3e00767dd52652319c9a93507b7
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-05-09 13:51:39 +0200

    less privileges, more env

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -8,6 +8,6 @@ if [ ! -f "/var/www/html/index.php" ]; then
 	while ! echo "SHOW DATABASES;" | mariadb -hmariadb -uboss -p${WP_BOSS_PASSWORD}; do sleep 1; done
 	wp config create --dbname=WP --dbuser=boss --dbpass=${WP_BOSS_PASSWORD} --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root 
 	wp core install --allow-root --url=${WP_URL} --admin_user=${WP_BOSS_LOGIN} --admin_password=${WP_BOSS_PASSWORD} --title=mywordpresssite --admin_email=pleasedonotemailme@42.fr
-	wp user create justauser pleasedonotemailme2@42.fr --allow-root --user_pass=userpw;
+	wp user create justauser pleasedonotemailme2@42.fr --allow-root --user_pass=${WP_USER_PASSWORD};
 fi
 /usr/sbin/php-fpm7.3 --nodaemonize
